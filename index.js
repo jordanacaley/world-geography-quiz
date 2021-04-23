2aa02706c6e674f3d638214c646030df7a1b47e1
Jordan Caley <jordancaley10@gmail.com>
2021-04-23 17:17:19 +0200

    Display question with options in random order

--- index.js
+++ index.js
@@ -7,12 +7,48 @@ axios
   .get(easyURL)
   .then((serverResponse) => {
     console.log(serverResponse.data.results);
-    displayInfo(serverResponse.data.results[8].question);
+    const results = serverResponse.data.results[0]; // to do: don't repeat a question already asked
+    const newQuestion = results.question; 
+    const correctAnswer = results.correct_answer;
+    const incorrectAnswers = results.incorrect_answers;
+    console.log(newQuestion);
+    console.log(correctAnswer);
+    console.log(incorrectAnswers);
+    displayQuestion(newQuestion);
+    displayOptions(correctAnswer, incorrectAnswers)
   })
   .catch((serverError) => {
     console.error(serverError);
   });
   
-function displayInfo(info) {
-  document.querySelector(".question").textContent = info;
+function displayQuestion(newQuestion) {
+  document.querySelector(".question").textContent = newQuestion;
+}
+
+// Fisher-Yates shuffle
+function shuffleArray(arr) {
+  let counter = arr.length;
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+    // And swap the last element with it
+    let temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+}
+
+function displayOptions(correctAnswer, incorrectAnswers) {
+  let optionsArray = [];
+  optionsArray.push(correctAnswer);
+  optionsArray.push(incorrectAnswers);
+  optionsArray = shuffleArray(optionsArray.flat()); // puts all 4 options into array and shuffles order
+  console.log(optionsArray);
+
+  let optionsCards = document.querySelectorAll('.option');  
+  for (let i = 0; i < optionsCards.length; i++) {
+    optionsCards[i].innerHTML = `<p>${optionsArray[i]}</p>`
+  }
 }
