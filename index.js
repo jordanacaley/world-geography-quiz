b338f3a7505d6d1229d76a532e34b35bc7b3d3aa
Jordan Caley <jordancaley10@gmail.com>
2021-04-27 16:11:36 +0200

    Write toggle functions and refactor code

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ for (let i = 0; i < 50; i++) {
 }
 
 // Initialize variables
+const myAudio = document.getElementById("beach-audio");
 const setLevelDiv = document.getElementById('set-level');
 const displayLevel = document.getElementById('display-level');
 const scoreboard = document.getElementById('scoreboard');
@@ -50,7 +51,6 @@ const qaDisplay = document.getElementById("qa-display");
 const difficultyBtns = document.querySelectorAll('.level');
 const earthAnimation = document.getElementById("earth");
 const timingDiv = document.getElementById("timing-div");
-const myAudio = document.getElementById("beach-audio");
 const reloadBtn = document.getElementById("reload")
 const optionsCards = document.querySelectorAll('.option');  
 const scoreboardCorrect = document.getElementById('correct');
@@ -58,42 +58,56 @@ const scoreboardIncorrect = document.getElementById('incorrect');
 const playAgainBtn = document.getElementById('play-again');
 const roundOverDisplay = document.getElementById('round-over-display');
 const finalResultMessage = document.getElementById('final-result-message');
+let difficulty = '';
 let correctAnswer = '';
 let incorrectAnswers = [];
-let difficulty = '';
 let counterCorrect = 0;
 let counterIncorrect = 0;
 
 difficultyBtns.forEach((btn) => (btn.onclick = setLevel));
+
+function toggleHidden(elem) {
+  elem.classList.toggle("hidden");
+}
+
+function removeHidden() {
+  reloadBtn.classList.remove("hidden");
+}
+
+function showHidden(elem) {
+  if (!elem.classList.contains("hidden")) {
+    elem.classList.toggle("hidden");
+  }
+}
+
+function hideElement(elem) {
+  if (elem.classList.contains("hidden")) {
+    elem.classList.toggle("hidden");
+  }
+}
 
 function setLevel(evt) {
   const level = evt.target.id;
   myAudio.play();
   if (level === 'easy') {
     difficulty = easyQuestions;
-    nextQuestion(easyQuestions);
   } else if (level === 'medium') {
     difficulty = mediumQuestions;
-    nextQuestion(mediumQuestions);
   } else {
     difficulty = hardQuestions;
-    nextQuestion(hardQuestions);
-  }
-  reloadBtn.classList.remove("hidden");
-  setLevelDiv.classList.toggle("hidden");
-  displayLevel.classList.toggle("hidden");
-  scoreboard.classList.toggle("hidden");
-  if (!earthAnimation.classList.contains("hidden")) {
-    earthAnimation.classList.toggle("hidden");
-  }
-  if (timingDiv.classList.contains("hidden")) {
-    timingDiv.classList.toggle("hidden");
   }
   displayLevel.textContent = `Difficulty: ${level.charAt(0).toUpperCase() + level.slice(1)}`;
   chronometer.startClick(printTime);
+  toggleHidden(setLevelDiv);
+  toggleHidden(displayLevel);
+  toggleHidden(scoreboard);
+  removeHidden();
+  showHidden(earthAnimation);
+  hideElement(timingDiv);
+  nextQuestion(difficulty);
 }
 
-function nextQuestion(setLevel) {    
+function nextQuestion(level) {    
   // Randomly select a number from the questionNumberArray, which will be the question asked
   let randomQuestion = questionNumberArray[Math.floor(Math.random()*questionNumberArray.length)];
 
@@ -104,15 +118,13 @@ function nextQuestion(setLevel) {
     }
   }
 
-  let results = setLevel[randomQuestion];
+  const results = level[randomQuestion];
   const newQuestion = results.question; 
   correctAnswer = results.correct_answer;
   incorrectAnswers = results.incorrect_answers;
   console.log(correctAnswer);
   console.log(incorrectAnswers);
-  if (qaDisplay.classList.contains("hidden")) {
-    qaDisplay.classList.toggle("hidden");
-  }
+  hideElement(qaDisplay);
   displayQuestion(newQuestion);
   displayOptions(correctAnswer, incorrectAnswers)
 }
@@ -155,17 +167,17 @@ function playAudio(url) {
 }
 
 function checkAnswer(evt) {
-  evt.target.blur();
   myAudio.pause();
+  evt.target.blur();
   const choice = evt.target.textContent;
   if (choice === correctAnswer) {
     counterCorrect += 1;
     playAudio("audio/lightapplause.mp3");
     scoreboardCorrect.textContent = `${counterCorrect}`;
     if (counterCorrect === 3) {
-      roundOverDisplay.classList.toggle("hidden");
+      toggleHidden(roundOverDisplay);
+      toggleHidden(qaDisplay);
       finalResultMessage.innerHTML = `<span>Woo hoo! 10 right <i class="fas fa-trophy"></i></span>`
-      qaDisplay.classList.toggle("hidden");
       chronometer.stopClick()
       printSplit(difficulty);
       myAudio.play();
@@ -177,9 +189,9 @@ function checkAnswer(evt) {
     playAudio("audio/fail-buzzer-03.mp3");
     scoreboardIncorrect.textContent = `${counterIncorrect}`;
     if (counterIncorrect === 3) {
-      roundOverDisplay.classList.toggle("hidden");
+      toggleHidden(roundOverDisplay);
+      toggleHidden(qaDisplay);
       finalResultMessage.innerHTML = `<span>Ouch! 3 wrong <i class="fas fa-user-injured"></i></span>`
-      qaDisplay.classList.toggle("hidden");
       chronometer.stopClick()
       myAudio.play();
     } else {
@@ -193,15 +205,11 @@ playAgainBtn.onclick = newGame;
 function newGame() {
   chronometer.resetClick();
   printTime();
-  setLevelDiv.classList.toggle("hidden");
-  displayLevel.classList.toggle("hidden");
-  scoreboard.classList.toggle("hidden");
-  if (!roundOverDisplay.classList.contains("hidden")) {
-    roundOverDisplay.classList.toggle("hidden");
-  }
-  if (!qaDisplay.classList.contains("hidden")) {
-    qaDisplay.classList.toggle("hidden");
-  }
+  toggleHidden(setLevelDiv);
+  toggleHidden(displayLevel);
+  toggleHidden(scoreboard);
+  showHidden(roundOverDisplay);
+  showHidden(qaDisplay);
   counterCorrect = 0;
   scoreboardCorrect.textContent = `${counterCorrect}`;
   counterIncorrect = 0;
@@ -217,6 +225,6 @@ function refreshPage(){
 } 
 
 // TO DO: 
-// Refactor code (function for hidden)
+// Refactor checkAnswer DRY
 // Fireworks or pop-up when you win/lose? Use z-index to move things back/forward, size from 0 to 100vh&vw, transition
 // Keep navbar? About page?
